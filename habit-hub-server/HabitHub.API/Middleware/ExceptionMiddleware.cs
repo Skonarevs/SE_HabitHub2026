507722d5889029cc4f1e6e09f6d7ce4b141455b1
Vladyslav Zelenkov <01193448@pw.edu.pl>
2026-03-24 17:46:59 +0100

    Custom errors

---
+++ habit-hub-server/HabitHub.API/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using HabitHub.API.Exceptions;
+
+namespace HabitHub.API.Middleware
+{
+    public class ExceptionMiddleware
+    {
+
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (AppException ex) 
+            {
+                await HandleExceptionAsync(context, ex.StatusCode, ex.Message);
+            }
+
+        }
+        private static async Task HandleExceptionAsync(HttpContext context, int statusCode, string message)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = statusCode;
+
+            var response = new
+            {
+                error = message,
+                statusCode = statusCode
+            };
+            await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+        }
+    }
+}
